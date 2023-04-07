a5b16bfbcce8db31325e89169b691c40a2a0b15f
Draiken7 <76527438+Draiken7@users.noreply.github.com>
2023-04-07 23:20:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ st.write("""
          """)
 
 st.subheader("Inputs:")
-st.write("Number with upto 4 decimal precision points are supported!")
+st.write("Number with upto 2 decimal precision points are supported!")
 
-x = st.number_input("Input Number 1:", value=0.0000, step=0.0001)
-y = st.number_input("Input Number 2:", value=0.0000, step=0.0001)
-z = st.number_input("Input Number 3:", value=0.0000, step=0.0001)
+x = st.number_input("Input Number 1:", value=0.0000, step=0.01)
+y = st.number_input("Input Number 2:", value=0.0000, step=0.01)
+z = st.number_input("Input Number 3:", value=0.0000, step=0.01)
 
 
 st.write(f'The Maximum of these values is:  {max(x, y, z)}')
