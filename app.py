47ade16b5d5b4cbc84cd99fa73553ea0b493f766
Draiken7 <76527438+Draiken7@users.noreply.github.com>
2023-04-07 22:15:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.write("""
+         # Maximum of Three Numbers
+         Displays the Maximum among the three inputs
+         """)
+
+st.subheader("Inputs:")
+st.write("Number with upto 4 decimal precision points are supported!")
+
+x = st.number_input("Input Number 1:", value=0.0000, step=0.0001)
+y = st.number_input("Input Number 2:", value=0.0000, step=0.0001)
+z = st.number_input("Input Number 2:", value=0.0000, step=0.0001)
+
+
+st.write(f'The Maximum of these values is:  {max(x, y, z)}')
